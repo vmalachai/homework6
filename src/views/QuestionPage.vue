cb63924a17349d3f64f88df468c095184ea9ff70
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-10-20 22:48:42 -0500

    Displaying answers

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -2,16 +2,38 @@
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
-
+import BaseTitle from '@/components/BaseTitle.vue';
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
+const answer = ref([])
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
+
+  answer.value.push({
+    id: answers.value.length,
+    correct: true,
+    answer: question.value.correct_answer
+  })
+
+  question.value.incorrect_answers.map((wrong_answer) => {
+    answer.value.push({
+      id: answers.value.length,
+      correct: false,
+    answer: wrong_answer
+  })
+  })
 })
 </script>
 
 \<template>
-  {{ question }}
+  <div v-if="question" class="">
+    <BaseTitle>{{ question.category }}</BaseTitle>
+    {{ question.question }}
+    <div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class=""></div>
+  </div>
+  <div v-else class="">
+    Loading...
+  </div>
 </template>
