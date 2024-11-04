af41fe34f56abdbf0aaf0320e8ab987a373acccd
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-11-03 20:22:12 -0600

    difficulty added

---
+++ src/components/DifficultyChip.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+    difficulty: {
+        type: String,
+        required: true,
+
+    }
+}) 
+</script>
+
+<template>
+    <p class="px-4 py-2 rounded-full border absolute uppercase text-white bottom-5 left-8 text-2x1 font-bold"
+    :class="props.difficulty === 'easy' ? 'bg-green-600' : props.difficulty === 'medium' ? 'bg-yellow-600' : 'bg-red-600'">
+{{ props.difficulty }}
+
+    </p>
+    </template>
--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,8 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
+
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
@@ -53,6 +55,7 @@ const shuffle = (array) => {
      class="bg-green-500 flex items-center justify-center text-4x1 rounded-lg text-white py-10 px-2">
     </div>
     </div>
+    <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
   </div>
   <div v-else class="">
     Loading...
